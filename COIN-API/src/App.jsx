d3cd906fe8eff671939e6fe981f90d78d40ec14b
wndud5814 <wndud5814@naver.com>
2023-11-10 15:15:51 +0900

    sa

--- COIN-API/src/App.jsx
+++ COIN-API/src/App.jsx
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const App = () => {
+  const getCoinPrices = async () => {
+    const response = await axios.get(
+      "https://api.upbit.com/v1/ticker?markets=KRW-BTC,KRW-ETH,KRW-MATIC"
+    );
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getCoinPrices();
+  }, []);
+
   return <div className="bg-red-100">Hello, React!</div>;
 };
 
